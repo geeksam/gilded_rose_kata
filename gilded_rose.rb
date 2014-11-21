18daa250a29682b2dd0a72bd5328bd4e4b29706b
Sam Livingston-Gray <samlg@samlg-laptop.local>
2014-11-20 16:45:15 -0800

    Move subclasses into another file

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -66,65 +66,7 @@ class UpdateItem
   end
 end
 
-class UpdateBrie < UpdateItem
-  def self.can_update?(item)
-    item.name == 'Aged Brie'
-  end
-
-  private
-
-  def adjust_quality
-    item.quality += 1
-  end
-
-  def adjust_quality_after_expiration
-    return unless expired?
-    item.quality += 1
-  end
-end
-
-class UpdateBackstagePasses < UpdateItem
-  def self.can_update?(item)
-    item.name == 'Backstage passes to a TAFKAL80ETC concert'
-  end
-
-  private
-
-  def adjust_quality
-    urgency_factor =
-      case item.sell_in
-      when (0..5)  ; 3
-      when (6..10) ; 2
-      else         ; 1
-      end
-    item.quality += urgency_factor
-  end
-
-  def adjust_quality_after_expiration
-    return unless expired?
-    item.quality = 0
-  end
-end
-
-class UpdateSulfuras < UpdateItem
-  def self.can_update?(item)
-    item.name == 'Sulfuras, Hand of Ragnaros'
-  end
-
-  private
-
-  def adjust_quality
-  end
-
-  def adjust_sell_in
-  end
-
-  def adjust_quality_after_expiration
-  end
-
-  def enforce_quality_constraints
-  end
-end
+require_relative 'update_item_strategies'
 
 # DO NOT CHANGE THINGS BELOW -----------------------------------------
 
