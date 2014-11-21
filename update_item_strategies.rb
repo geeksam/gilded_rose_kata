f44640081f733636c40896b8c61fe6ca8ce74f38
Sam Livingston-Gray <samlg@samlg-laptop.local>
2014-11-20 16:11:04 -0800

    Move subclasses into another file
    (This is purely to facilitate split-screen editing.)

---
+++ update_item_strategies.rb
@@ -0,0 +1,63 @@
+class UpdateBrie < UpdateItem
+  def self.can_update?(item)
+    item.name == 'Aged Brie'
+  end
+
+  private
+
+  def unexpired_quality_adjustment
+    1
+  end
+
+  def expired_quality_adjustment
+    1
+  end
+end
+
+class UpdateBackstagePasses < UpdateItem
+  def self.can_update?(item)
+    item.name == 'Backstage passes to a TAFKAL80ETC concert'
+  end
+
+  private
+
+  def unexpired_quality_adjustment
+    urgency_factor
+  end
+
+  def expired_quality_adjustment
+    -1 * item.quality # additive inverse
+  end
+
+  def urgency_factor
+    case item.sell_in
+    when (0..5)  ; 3
+    when (6..10) ; 2
+    else         ; 1
+    end
+  end
+end
+
+class UpdateSulfuras < UpdateItem
+  def self.can_update?(item)
+    item.name == 'Sulfuras, Hand of Ragnaros'
+  end
+
+  private
+
+  def unexpired_quality_adjustment
+    0
+  end
+
+  def aging_adjustment
+    0
+  end
+
+  def expired_quality_adjustment
+    0
+  end
+
+  def enforce_quality_constraints
+  end
+end
+
