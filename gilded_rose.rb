66511edcfca1b0279d93288ef7b1cb860a5cb9c8
Sam Livingston-Gray <geeksam@gmail.com>
2017-12-09 13:58:54 -1000

    Remove non-backstage logic from backstage updater

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -22,36 +22,24 @@ end
 
 class BackstagePassItemUpdater
   def update_item(item)
-    if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        item.quality -= 1
+    if item.quality < 50
+      item.quality += 1
+      if item.sell_in < 11
+        if item.quality < 50
+          item.quality += 1
+        end
       end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
+      if item.sell_in < 6
+        if item.quality < 50
+          item.quality += 1
         end
       end
     end
+
     item.sell_in -= 1
+
     if item.sell_in < 0
-      if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          item.quality -= 1
-        end
-      else
-        item.quality = item.quality - item.quality
-      end
+      item.quality = item.quality - item.quality
     end
   end
 end
